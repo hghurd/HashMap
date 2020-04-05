e074244d02c230d7932f1c4d267e1788dd6bbd9a
hhurd <hghurd2001@gmail.com>
2020-04-05 13:31:08 -0400

    Implemented the SimpleMap interface

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,2 +1,58 @@
-public class HashMap {
+import java.util.List;
+
+public class HashMap implements SimpleMap {
+    @Override
+    public int hashFn(String key) {
+        return 0;
+    }
+
+    @Override
+    public Integer get(String key) {
+        return null;
+    }
+
+    @Override
+    public Integer put(String key, Integer value) {
+        return null;
+    }
+
+    @Override
+    public Integer remove(String key) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
+
+    @Override
+    public double loadFactor() {
+        return 0;
+    }
+
+    @Override
+    public int loadVariance() {
+        return 0;
+    }
+
+    @Override
+    public List<Entry> entryList() {
+        return null;
+    }
+
+    @Override
+    public List<Entry>[] hashArray() {
+        return new List[0];
+    }
 }
